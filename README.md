faff8cc466b3a77ff9f9805a3a9cb109ed3744df
yut_fut <seregey.golubev@mail.ru>
2021-07-27 18:06:50 +0300

    Initial project version

---
+++ README.md
@@ -0,0 +1,5 @@
+You can install or upgrade python-telegram-bot with:
+
+```
+pip install python-telegram-bot --upgrade
+```
---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+
+from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
+
+from dotenv import load_dotenv
+load_dotenv()
+
+token = os.getenv('TOKEN')
+
+updater = Updater(token='1897406367:AAGiEc8IetFxqnV1RZdyrcCYQEaoFbCuG7U')
+
+from openapi_client import openapi
+from datetime import datetime, timedelta
+from pytz import timezone
+
+client = openapi.sandbox_api_client(token)
+client.sandbox.sandbox_register_post()
+client.sandbox.sandbox_clear_post()
+client.sandbox.sandbox_currencies_balance_post(sandbox_set_currency_balance_request={"currency": "USD", "balance": 1000})
+
+
+def print_orders():
+    orders = client.orders.orders_get()
+    print("active orders")
+    print(orders)
+    print()
+    return orders
+
+
+def say_hi(update, context):
+    chat = update.effective_chat
+    orders = print_orders()
+    context.bot.send_message(chat_id=chat.id, text=orders)
+
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='Спасибо, что включили меня')
+
+
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+
+updater.start_polling()
